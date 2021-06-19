de708e62f84eb03f879c6948200708eb96169a6c
Lloen <lloen@ymail.com>
2021-06-19 17:28:39 +0200

    Changed naming from deauth ro mitm

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from global_variables import GlobalData
 from main_arp import GUI_MAIN_ARP
 from main_discover import GUI_DISCOVER
-from main_deauth import GUI_DEAUTH
+from main_mitm import GUI_MITM
 
 
 def main():
     _var = GlobalData()
     GUI_MAIN_ARP(_var)
     GUI_DISCOVER(_var)
-    GUI_DEAUTH(_var)
+    GUI_MITM(_var)
 
 
 if __name__ == '__main__':
