d9dc8ba7686e59bef454c03b14a6f00a2bef0115
Lloen <lloen@ymail.com>
2021-06-20 16:38:25 +0200

    Changed renamed variable

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from global_variables import GlobalData
-from main_arp import GUI_MAIN_ARP
+from main_iface import GUI_MAIN_IFACE
 from main_discover import GUI_DISCOVER
 from main_mitm import GUI_MITM
 
 
 def main():
     _var = GlobalData()
-    GUI_MAIN_ARP(_var)
+    GUI_MAIN_IFACE(_var)
     GUI_DISCOVER(_var)
     GUI_MITM(_var)
 
