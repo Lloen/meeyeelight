12039bb3726c577cc71700bc63a73159fab99690
Lloen <lloen@ymail.com>
2021-06-22 17:27:20 +0200

    Added create AP function and added channel number for Malicious Yeelight AP

--- hotspot/hotspot_controller.py
+++ hotspot/hotspot_controller.py
@@ -1,15 +1,15 @@
 import os
 
-def create_hotspot(iface, ssid, gateway_ip, new_mac):
+def create_hotspot(iface, ssid, password):
+    create_hotspot_cmd = f"create_ap -n {iface} {ssid} {password} --no-virt"
+    os.system(create_hotspot_cmd)
+
+def create_malicious_yeelight(iface, ssid, gateway_ip, new_mac):
     iface_down_cmd = f"sudo ifconfig {iface} down"
     iface_change_mac_cmd = f"sudo ifconfig {iface} hw ether {new_mac}"
     iface_up_cmd = f"sudo ifconfig {iface} up"
-    create_hotspot_cmd = f"create_ap -g {gateway_ip} -n {iface} {ssid} --no-virt"
+    create_malicious_yeelight_cmd = f"create_ap -c 10 -g {gateway_ip} -n {iface} {ssid} --no-virt"
     os.system(iface_down_cmd)
     os.system(iface_change_mac_cmd)
     os.system(iface_up_cmd)
-    os.system(create_hotspot_cmd)
-
-def stop_hotspot(iface):
-    stop_hotspot_cmd = "sudo nmcli dev wifi hotspot ifname " + iface + " ssid " + ssid +" password '" + password +"'"
-    os.system(stop_hotspot_cmd)
+    os.system(create_malicious_yeelight_cmd)
