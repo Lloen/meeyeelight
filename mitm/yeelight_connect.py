8392600668fee2ca0b216a1dec0c6ac8b9a88e9a
Lloen <lloen@ymail.com>
2021-06-22 17:28:56 +0200

    Added to specific Yeelight AP channel to connect and keep on trying to connect to Yeelight AP

--- mitm/yeelight_connect.py
+++ mitm/yeelight_connect.py
@@ -1,9 +1,13 @@
 import os
 
 def connect_to_yeelight(ssid, iface):
+    iface_channel = f"sudo iwconfig {iface} channel 6"
+    os.system(iface_channel)
+    connect_yeelight_cmd = f"nmcli d wifi connect {ssid} ifname {iface}"
     try:
-        os.system(f"nmcli d wifi connect {ssid} ifname {iface}")
+        if os.system(connect_yeelight_cmd) != 0:
+            raise Exception()
     except:
-        raise
+        connect_to_yeelight(ssid, iface)
     else:
         return True
